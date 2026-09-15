7b123cddd89f9d06c0f8735ec3360919c67b608f
Subrata Mondal <subratamondal@tutanota.com>
2026-09-16 00:48:30 +0530

    feat(todo): display "no todos found" message

--- internal/todo/logger.go
+++ internal/todo/logger.go
@@ -12,12 +12,17 @@ func PrintDataTable(todos *[]database.Todo) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 	defer w.Flush()
 
+	if len(*todos) == 0 {
+		fmt.Fprintln(w, "No todos found.")
+		return
+	}
+
 	fmt.Fprintln(w, "ID\tTITLE\tCOMPLETED")
 	for _, todo := range *todos {
 		fmt.Fprintf(w, "%d\t%s\t%t\n",
 			todo.ID,
 			todo.Title,
-			todo.Completed,
+			todo.Completed.Bool,
 		)
 	}
 }

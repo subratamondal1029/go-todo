db3019f0e3226638d615fe3f8ee672a1e1b923a2
Subrata Mondal <subratamondal@tutanota.com>
2026-09-16 01:06:16 +0530

    feat(build): add build script for application

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running database migrations..."
+dbmate up
+
+echo "Building application..."
+mkdir -p bin
+GOOS=linux GOARCH=amd64 go build -o bin/todo cmd/todo/main.go
+
+echo "Application built successfully."

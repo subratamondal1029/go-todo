1c976e7f39136c59001edb089d0cda81e98fcb89
Subrata Mondal <subratamondal@tutanota.com>
2026-09-16 14:06:38 +0530

    refactor(build): specify database url for migrations

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "Running database migrations..."
-dbmate up
+DATABASE_URL=sqlite:internal/cli/assets/generated/todos.db dbmate up
 
 echo "Building application..."
 mkdir -p bin
